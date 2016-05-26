ff4364cf83c23901a309363bbddd52ffb64b3498
Edward Ash <cazador481@users.noreply.github.com>
2016-05-26 13:27:15 -0700

    aliases

--- aliases.zsh
+++ aliases.zsh
@@ -5,13 +5,13 @@ alias dc='cd'
 
 alias -- vnc_setup='vncserver -geometry 5012x960 -randr 5012x980,3840x1080,1280x1024,1680x1050,1920x1200,1040x1050'
 alias s_zsh='zgen reset;source $ZDOTDIR/.zshrc'
-# which () { alias | /usr/intel/00r1/bin/gwhich -i $* }
 
 #open with
 alias -s vh=$EDITOR
 alias -s vs=$EDITOP 
 alias -s log=$EDITOR
 alias -s out=$EDITOR
+alias -s pm=$EDITOR
 
 #git aliases
 alias root='cd $(git rev-parse --show-cdup || echo ".")'
